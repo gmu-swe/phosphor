2f73d8c31eaff2815a25727dbdac5115bf1c07af
Jonathan Bell <jbell@cs.columbia.edu>
2015-12-14 10:47:46 -0500

    Add missing files

---
+++ Phosphor/src/java/lang/Thread.java
@@ -0,0 +1,7 @@
+package java.lang;
+
+public class Thread {
+    public static Object[] preallocReturns;
+    public static native Thread currentThread();
+
+}
---
+++ Phosphor/src/java/lang/reflect/Array.java
@@ -0,0 +1,23 @@
+package java.lang.reflect;
+
+public class Array {
+	public static int getLength(Object ar) {
+		return 0;
+	}
+
+	public static Object newInstance(Class<?> componentType, int length) throws NegativeArraySizeException {
+		return null;
+	}
+
+	public static Object newInstance(Class<?> componentType, int... dimensions) throws IllegalArgumentException, NegativeArraySizeException {
+		return null;
+	}
+	public static Object get(Object ar, int idx)
+	{
+		return null;
+	}
+	public static void set(Object ar, int idx, Object v)
+	{
+
+	}
+}
