2253a579e4ab88638b0a29b5ec7aa18bf1857add
dongmu <wangzilinux@gmail.com>
2016-04-20 10:43:31 -0400

    format Configuration.java

--- Phosphor/src/edu/columbia/cs/psl/phosphor/Configuration.java
+++ Phosphor/src/edu/columbia/cs/psl/phosphor/Configuration.java
@@ -19,8 +19,8 @@ import edu.columbia.cs.psl.phosphor.runtime.Taint;
 public class Configuration {
 	public static String ADDL_IGNORE = null;
 	public static boolean MULTI_TAINTING = true;
-	public static boolean IMPLICIT_TRACKING = true; //must be set to TRUE for MULTI_TAINTING to work!
-	public static boolean DATAFLOW_TRACKING = true; //default
+	public static boolean IMPLICIT_TRACKING = true; // must be set to TRUE for MULTI_TAINTING to work!
+	public static boolean DATAFLOW_TRACKING = true; // default
 	public static boolean ARRAY_LENGTH_TRACKING = false;
 	public static boolean WITH_ENUM_BY_VAL = false;
 	public static boolean WITH_UNBOX_ACMPEQ = false;
@@ -36,20 +36,42 @@ public class Configuration {
 	/*
 	 * Derived configuration values
 	 */
-	public static String TAINT_TAG_DESC = (MULTI_TAINTING ? "Ledu/columbia/cs/psl/phosphor/runtime/Taint;" : "I");
-	public static String TAINT_TAG_ARRAYDESC = (MULTI_TAINTING ? "[Ledu/columbia/cs/psl/phosphor/runtime/Taint;" : "[I");
-	public static String TAINT_TAG_INTERNAL_NAME = (MULTI_TAINTING ? "edu/columbia/cs/psl/phosphor/runtime/Taint" : null);
-	public static String TAINT_TAG_ARRAY_INTERNAL_NAME = (MULTI_TAINTING ? "[Ledu/columbia/cs/psl/phosphor/runtime/Taint;" : "[I");
-	public static int NULL_TAINT_LOAD_OPCODE = (MULTI_TAINTING ? Opcodes.ACONST_NULL : Opcodes.ICONST_0);
-	public static Object TAINT_TAG_STACK_TYPE = (MULTI_TAINTING ? "edu/columbia/cs/psl/phosphor/runtime/Taint" : Opcodes.INTEGER);
+	public static String TAINT_TAG_DESC = (MULTI_TAINTING
+			? "Ledu/columbia/cs/psl/phosphor/runtime/Taint;"
+			: "I");
+	public static String TAINT_TAG_ARRAYDESC = (MULTI_TAINTING
+			? "[Ledu/columbia/cs/psl/phosphor/runtime/Taint;"
+			: "[I");
+	public static String TAINT_TAG_INTERNAL_NAME = (MULTI_TAINTING
+			? "edu/columbia/cs/psl/phosphor/runtime/Taint"
+			: null);
+	public static String TAINT_TAG_ARRAY_INTERNAL_NAME = (MULTI_TAINTING
+			? "[Ledu/columbia/cs/psl/phosphor/runtime/Taint;"
+			: "[I");
+	public static int NULL_TAINT_LOAD_OPCODE = (MULTI_TAINTING
+			? Opcodes.ACONST_NULL
+			: Opcodes.ICONST_0);
+	public static Object TAINT_TAG_STACK_TYPE = (MULTI_TAINTING
+			? "edu/columbia/cs/psl/phosphor/runtime/Taint"
+			: Opcodes.INTEGER);
 	public static Object TAINT_TAG_ARRAY_STACK_TYPE = TAINT_TAG_ARRAY_INTERNAL_NAME;
 	public static String MULTI_TAINT_HANDLER_CLASS = "edu/columbia/cs/psl/phosphor/runtime/Taint";
-	public static String TAINTED_INT_INTERNAL_NAME = (!MULTI_TAINTING ? "edu/columbia/cs/psl/phosphor/struct/TaintedIntWithIntTag" : "edu/columbia/cs/psl/phosphor/struct/TaintedIntWithObjTag");
+	public static String TAINTED_INT_INTERNAL_NAME = (MULTI_TAINTING
+			? "edu/columbia/cs/psl/phosphor/struct/TaintedIntWithObjTag"
+			: "edu/columbia/cs/psl/phosphor/struct/TaintedIntWithIntTag");
 	public static String TAINTED_INT_DESC = "L" + TAINTED_INT_INTERNAL_NAME + ";";
-	public static int TAINT_ARRAY_LOAD_OPCODE = (!MULTI_TAINTING ? Opcodes.IALOAD : Opcodes.AALOAD);
-	public static int TAINT_ARRAY_STORE_OPCODE = (!MULTI_TAINTING ? Opcodes.IASTORE : Opcodes.AASTORE);
-	public static int TAINT_LOAD_OPCODE = (!MULTI_TAINTING ? Opcodes.ILOAD : Opcodes.ALOAD);
-	public static int TAINT_STORE_OPCODE = (!MULTI_TAINTING ? Opcodes.ISTORE : Opcodes.ASTORE);
+	public static int TAINT_ARRAY_LOAD_OPCODE = (MULTI_TAINTING
+			? Opcodes.AALOAD
+			: Opcodes.IALOAD);
+	public static int TAINT_ARRAY_STORE_OPCODE = (MULTI_TAINTING
+			? Opcodes.AASTORE
+			: Opcodes.IASTORE);
+	public static int TAINT_LOAD_OPCODE = (MULTI_TAINTING
+			? Opcodes.ALOAD
+			: Opcodes.ILOAD);
+	public static int TAINT_STORE_OPCODE = (MULTI_TAINTING
+			? Opcodes.ASTORE
+			: Opcodes.ISTORE);
 	public static boolean OPT_CONSTANT_ARITHMETIC = !IMPLICIT_TRACKING;
 	public static Class TAINT_TAG_OBJ_CLASS = (Taint.class);
 	public static Class TAINT_TAG_OBJ_ARRAY_CLASS = (Taint[].class);
@@ -61,42 +83,79 @@ public class Configuration {
 	public static DerivedTaintListener derivedTaintListener;
 	public static String CACHE_DIR = null;
 
+
 	public static void init() {
-		TAINT_TAG_DESC = (MULTI_TAINTING ? "Ledu/columbia/cs/psl/phosphor/runtime/Taint;" : "I");
-		TAINT_TAG_ARRAYDESC = (MULTI_TAINTING ? "[Ledu/columbia/cs/psl/phosphor/runtime/Taint;" : "[I");
-		TAINT_TAG_INTERNAL_NAME = (MULTI_TAINTING ? "edu/columbia/cs/psl/phosphor/runtime/Taint" : null);
-		TAINT_TAG_ARRAY_INTERNAL_NAME = (MULTI_TAINTING ? "[Ledu/columbia/cs/psl/phosphor/runtime/Taint;" : "[I");
-		NULL_TAINT_LOAD_OPCODE = (MULTI_TAINTING ? Opcodes.ACONST_NULL : Opcodes.ICONST_0);
-		TAINT_TAG_STACK_TYPE = (MULTI_TAINTING ? "edu/columbia/cs/psl/phosphor/runtime/Taint" : Opcodes.INTEGER);
+		TAINT_TAG_DESC = (MULTI_TAINTING
+				? "Ledu/columbia/cs/psl/phosphor/runtime/Taint;"
+				: "I");
+		TAINT_TAG_ARRAYDESC = (MULTI_TAINTING
+				? "[Ledu/columbia/cs/psl/phosphor/runtime/Taint;"
+				: "[I");
+		TAINT_TAG_INTERNAL_NAME = (MULTI_TAINTING
+				? "edu/columbia/cs/psl/phosphor/runtime/Taint"
+				: null);
+		TAINT_TAG_ARRAY_INTERNAL_NAME = (MULTI_TAINTING
+				? "[Ledu/columbia/cs/psl/phosphor/runtime/Taint;"
+				: "[I");
+		NULL_TAINT_LOAD_OPCODE = (MULTI_TAINTING
+				? Opcodes.ACONST_NULL
+				: Opcodes.ICONST_0);
+		TAINT_TAG_STACK_TYPE = (MULTI_TAINTING
+				? "edu/columbia/cs/psl/phosphor/runtime/Taint"
+				: Opcodes.INTEGER);
 		TAINT_TAG_ARRAY_STACK_TYPE = TAINT_TAG_ARRAY_INTERNAL_NAME;
 		MULTI_TAINT_HANDLER_CLASS = "edu/columbia/cs/psl/phosphor/runtime/Taint";
-		TAINTED_INT_INTERNAL_NAME = (!MULTI_TAINTING ? "edu/columbia/cs/psl/phosphor/struct/TaintedIntWithIntTag" : "edu/columbia/cs/psl/phosphor/struct/TaintedIntWithObjTag");
+		TAINTED_INT_INTERNAL_NAME = (MULTI_TAINTING
+				? "edu/columbia/cs/psl/phosphor/struct/TaintedIntWithObjTag"
+				: "edu/columbia/cs/psl/phosphor/struct/TaintedIntWithIntTag");
 		TAINTED_INT_DESC = "L" + TAINTED_INT_INTERNAL_NAME + ";";
-		TAINT_ARRAY_LOAD_OPCODE = (!MULTI_TAINTING ? Opcodes.IALOAD : Opcodes.AALOAD);
-		TAINT_ARRAY_STORE_OPCODE = (!MULTI_TAINTING ? Opcodes.IASTORE : Opcodes.AASTORE);
-		TAINT_LOAD_OPCODE = (!MULTI_TAINTING ? Opcodes.ILOAD : Opcodes.ALOAD);
-		TAINT_STORE_OPCODE = (!MULTI_TAINTING ? Opcodes.ISTORE : Opcodes.ASTORE);
+		TAINT_ARRAY_LOAD_OPCODE = (MULTI_TAINTING
+				? Opcodes.AALOAD
+				: Opcodes.IALOAD);
+		TAINT_ARRAY_STORE_OPCODE = (MULTI_TAINTING
+				? Opcodes.AASTORE
+				: Opcodes.IASTORE);
+		TAINT_LOAD_OPCODE = (MULTI_TAINTING
+				? Opcodes.ALOAD
+				: Opcodes.ILOAD);
+		TAINT_STORE_OPCODE = (MULTI_TAINTING
+				? Opcodes.ASTORE
+				: Opcodes.ISTORE);
 		OPT_CONSTANT_ARITHMETIC = !IMPLICIT_TRACKING;
-		TAINT_TAG_OBJ_ARRAY_CLASS = (MULTI_TAINTING ? Taint[].class : int[].class);
-		TAINT_TAG_OBJ_CLASS = (MULTI_TAINTING ? Taint.class : Integer.TYPE);
+		TAINT_TAG_OBJ_ARRAY_CLASS = (MULTI_TAINTING
+				? Taint[].class
+				:int[].class);
+		TAINT_TAG_OBJ_CLASS = (MULTI_TAINTING
+				? Taint.class
+				: Integer.TYPE);
 		
 		if(WITH_SELECTIVE_INST)
 			GENERATE_UNINST_STUBS = true;
 
-		if (TaintTrackingClassVisitor.class != null && TaintTrackingClassVisitor.class.getClassLoader() != null) {
-			URL r = TaintTrackingClassVisitor.class.getClassLoader().getResource("phosphor-mv");
+		if (TaintTrackingClassVisitor.class != null
+				&& TaintTrackingClassVisitor.class.getClassLoader() != null) {
+
+			URL r = TaintTrackingClassVisitor.class
+				.getClassLoader().getResource("phosphor-mv");
+
 			if (r != null)
 				try {
 					Properties props = new Properties();
 					props.load(r.openStream());
 					if (props.containsKey("extraMV"))
-						extensionMethodVisitor = (Class<? extends TaintAdapter>) Class.forName(props.getProperty("extraMV"));
+						extensionMethodVisitor =
+							(Class<? extends TaintAdapter>)
+							Class.forName(props.getProperty("extraMV"));
 					if (props.containsKey("extraCV"))
-						extensionClassVisitor = (Class<? extends ClassVisitor>) Class.forName(props.getProperty("extraCV"));
+						extensionClassVisitor =
+							(Class<? extends ClassVisitor>)
+							Class.forName(props.getProperty("extraCV"));
 					if (props.containsKey("taintTagFactory"))
-						taintTagFactory = (TaintTagFactory) Class.forName(props.getProperty("taintTagFactory")).newInstance();
+						taintTagFactory = (TaintTagFactory)
+							Class.forName(props.getProperty("taintTagFactory")).newInstance();
 					if (props.containsKey("derivedTaintListener"))
-						derivedTaintListener = (DerivedTaintListener) Class.forName(props.getProperty("derivedTaintListener")).newInstance();
+						derivedTaintListener = (DerivedTaintListener)
+							Class.forName(props.getProperty("derivedTaintListener")).newInstance();
 				} catch (IOException ex) {
 					//fail silently
 				} catch (ClassNotFoundException e) {
