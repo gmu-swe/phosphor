03ec08a06505ca610f6e8eb36cf43b5a0f2ab2ce
dongmu <wangzilinux@gmail.com>
2016-04-20 12:07:21 -0400

    format DataAndControlFlowTagFactory.java and TaintTagFactory.java

--- Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/TaintTagFactory.java
+++ Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/TaintTagFactory.java
@@ -13,13 +13,21 @@ public interface TaintTagFactory {
 	public boolean isIgnoredClass(String classname);
 	public void generateEmptyTaint(MethodVisitor mv);
 	public void generateEmptyTaintArray(Object[] array, int dimensions);
-	public void stackOp(int opcode, MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
-	public void jumpOp(int opcode, int branchStarting, Label label, MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
-	public void typeOp(int opcode, String type, MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
-	public void iincOp(int var, int increment, MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
-	public void intOp(int opcode, int arg, MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
+	
+	public void stackOp(int opcode,
+			MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
+	public void jumpOp(int opcode, int branchStarting, Label label,
+			MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
+	public void typeOp(int opcode, String type,
+			MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
+	public void iincOp(int var, int increment,
+			MethodVisitor mv, LocalVariableManager lvs,	TaintPassingMV ta);
+	public void intOp(int opcode, int arg,
+			MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
 	public void signalOp(int signal, Object option);
-	public void fieldOp(int opcode, String owner, String name, String desc, MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
-	public void methodEntered(String owner, String name, String desc, MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
+	public void fieldOp(int opcode, String owner, String name, String desc,
+			MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
+	public void methodEntered(String owner, String name, String desc,
+			MethodVisitor mv, LocalVariableManager lvs, TaintPassingMV ta);
 	public void lineNumberVisited(int line);
 }
