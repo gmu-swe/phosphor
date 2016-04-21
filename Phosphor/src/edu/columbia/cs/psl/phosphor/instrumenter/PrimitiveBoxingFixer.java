b2dbbc789122c68549850414d499bf4eec9b4416
dongmu <wangzilinux@gmail.com>
2016-04-21 11:44:37 -0400

    format PrimitiveBoxingFixer.java and StringTaintVerifyingMV.java

--- Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/PrimitiveBoxingFixer.java
+++ Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/PrimitiveBoxingFixer.java
@@ -14,13 +14,15 @@ import edu.columbia.cs.psl.phosphor.runtime.TaintSentinel;
 
 public class PrimitiveBoxingFixer extends TaintAdapter implements Opcodes {
 
-	public PrimitiveBoxingFixer(int api, String className, String name, String desc, String signature, String[] exceptions, MethodVisitor mv, NeverNullArgAnalyzerAdapter analyzer) {
-		super(api, className, name, desc, signature, exceptions, mv, analyzer);
-	}
-
 	int tmpInt = -1;;
 	boolean ignoreLoadingTaint;
+	boolean followedByFrame = false;
 
+	public PrimitiveBoxingFixer(int api, String className, String name, String desc,
+			String signature, String[] exceptions,
+			MethodVisitor mv, NeverNullArgAnalyzerAdapter analyzer) {
+		super(api, className, name, desc, signature, exceptions, mv, analyzer);
+	}
 
 	@Override
 	public void visitVarInsn(int opcode, int var) {
@@ -28,6 +30,7 @@ public class PrimitiveBoxingFixer extends TaintAdapter implements Opcodes {
 		if(followedByFrame)
 			followedByFrame = false;
 	}
+
 	@Override
 	public void visitFieldInsn(int opcode, String owner, String name, String desc) {
 		super.visitFieldInsn(opcode, owner, name, desc);
@@ -41,31 +44,35 @@ public class PrimitiveBoxingFixer extends TaintAdapter implements Opcodes {
 		if(followedByFrame)
 			followedByFrame = false;
 	}
+	
 	@Override
 	public void visitLdcInsn(Object cst) {
 		super.visitLdcInsn(cst);
 		if(followedByFrame)
 			followedByFrame = false;
 	}
+
 	@Override
 	public void visitTypeInsn(int opcode, String type) {
 		super.visitTypeInsn(opcode, type);
 		if(followedByFrame)
 			followedByFrame = false;
 	}
+
 	@Override
 	public void visitLookupSwitchInsn(Label dflt, int[] keys, Label[] labels) {
 		super.visitLookupSwitchInsn(dflt, keys, labels);
 		if(followedByFrame)
 			followedByFrame = false;
 	}
+
 	@Override
 	public void visitTableSwitchInsn(int min, int max, Label dflt, Label... labels) {
 		super.visitTableSwitchInsn(min, max, dflt, labels);
 		if(followedByFrame)
 			followedByFrame = false;
 	}
-	boolean followedByFrame = false;
+	
 	@Override
 	public void visitInsn(int opcode) {
 		if (opcode == TaintUtils.DONT_LOAD_TAINT)
@@ -88,23 +95,27 @@ public class PrimitiveBoxingFixer extends TaintAdapter implements Opcodes {
 		for (Type t : Type.getArgumentTypes(desc))
 			if (t.getSort() == Type.OBJECT && t.getDescriptor().equals("Ljava/lang/String;"))
 				argIsStr = true;
-		//Get an extra copy of the taint
-		if(Configuration.WITH_ENUM_BY_VAL && opcode == INVOKESTATIC && owner.equals(Type.getInternalName(Enum.class)))
-		{
-			super.visitMethodInsn(opcode, Type.getInternalName(TaintUtils.class), "enumValueOf", desc,itfc);
+		// Get an extra copy of the taint
+		if (Configuration.WITH_ENUM_BY_VAL
+				&& opcode == INVOKESTATIC
+				&& owner.equals(Type.getInternalName(Enum.class))) {
+			super.visitMethodInsn(opcode,
+					Type.getInternalName(TaintUtils.class),
+					"enumValueOf",
+					desc,
+					itfc);
 			return;
-		}
-		else if ((owner.equals(Type.getInternalName(Integer.class))
-		//				|| owner.equals(Type.getInternalName(Byte.class))
-		//				|| owner.equals(Type.getInternalName(Character.class))
-		//				|| owner.equals(Type.getInternalName(Short.class)) ||  owner.equals(Type.getInternalName(Float.class)) 
-				|| owner.equals(Type.getInternalName(Long.class)) || owner.equals(Type.getInternalName(Double.class))) && name.equals("valueOf$$PHOSPHORTAGGED") && nArgs == 2 && !argIsStr) {
+		} else if ((owner.equals(Type.getInternalName(Integer.class))
+					|| owner.equals(Type.getInternalName(Long.class))
+					|| owner.equals(Type.getInternalName(Double.class)))
+				&& name.equals("valueOf$$PHOSPHORTAGGED")
+				&& nArgs == 2
+				&& !argIsStr) {
 			Type argT = Type.getArgumentTypes(desc)[1];
 			int argSize = argT.getSize();
 			if (argSize == 1) {
-//				System.out.println(analyzer.stack);
-				//stack is currently T I <top>
-				//we'll support (Integer) 1 == (Integer) 1 as long as there is no taint on it.
+				// stack is currently T I <top>
+				// we'll support (Integer) 1 == (Integer) 1 as long as there is no taint on it.
 				super.visitInsn(SWAP);
 				FrameNode fn = getCurrentFrameNode();
 				super.visitInsn(DUP);
@@ -124,7 +135,14 @@ public class PrimitiveBoxingFixer extends TaintAdapter implements Opcodes {
 				super.visitInsn(Opcodes.POP2);
 				//N N T I
 				super.visitInsn(Opcodes.ACONST_NULL);
-				super.visitMethodInsn(Opcodes.INVOKESPECIAL, owner, "<init>", "("+Configuration.TAINT_TAG_DESC + Type.getArgumentTypes(desc)[1].getDescriptor() + Type.getDescriptor(TaintSentinel.class) + ")V",false);
+				super.visitMethodInsn(Opcodes.INVOKESPECIAL,
+						owner,
+						"<init>",
+						"(" + Configuration.TAINT_TAG_DESC
+							+ Type.getArgumentTypes(desc)[1].getDescriptor()
+							+ Type.getDescriptor(TaintSentinel.class)
+							+ ")V",
+						false);
 				FrameNode fn2 = getCurrentFrameNode();
 				super.visitLabel(isOK);
 				if(!followedByFrame)
@@ -162,74 +180,28 @@ public class PrimitiveBoxingFixer extends TaintAdapter implements Opcodes {
 				super.visitVarInsn(argT.getOpcode(ILOAD), tmp);
 
 				super.visitInsn(Opcodes.ACONST_NULL);
-				super.visitMethodInsn(Opcodes.INVOKESPECIAL, owner, "<init>", "("+Configuration.TAINT_TAG_DESC + Type.getArgumentTypes(desc)[1].getDescriptor() + Type.getDescriptor(TaintSentinel.class) + ")V", false);
+				super.visitMethodInsn(Opcodes.INVOKESPECIAL,
+						owner,
+						"<init>",
+						"(" + Configuration.TAINT_TAG_DESC
+							+ Type.getArgumentTypes(desc)[1].getDescriptor()
+							+ Type.getDescriptor(TaintSentinel.class)
+							+ ")V",
+						false);
 				lvs.freeTmpLV(tmp);
 				lvs.freeTmpLV(tmpT);
-        FrameNode fn2 = getCurrentFrameNode();
+				FrameNode fn2 = getCurrentFrameNode();
 				super.visitLabel(isOK);
 				if(!followedByFrame)
 					acceptFn(fn2);
-//				super.visitMethodInsn(opcode, owner, name, desc,itfc);
-
+				// super.visitMethodInsn(opcode, owner, name, desc,itfc);
 			}
-		} 
-//		else if (owner.equals(Type.getInternalName(Integer.class)) && name.equals("parseInt$$PHOSPHORTAGGED")) {
-//			if (nArgs == 2) {
-//				super.visitInsn(Opcodes.DUP);
-//				super.visitMethodInsn(opcode, owner, name, desc,itfc);
-//				super.visitInsn(DUP_X1);
-//				super.visitInsn(SWAP);
-//				retrieveTopOfStackTaintAndPop();
-//			} else if (nArgs == 4) {
-//				//S I I
-//				super.visitInsn(DUP2_X1);
-//				//				//I I S II
-//				super.visitInsn(POP2);
-//				//				//II S
-//				retrieveTopOfStackTaint(true, true);
-//				int tmpInt = lvs.getTmpLV(Type.INT_TYPE);
-//
-//				super.visitVarInsn(ISTORE, tmpInt);
-//				super.visitInsn(DUP_X2);
-//				super.visitInsn(POP);
-//				super.visitMethodInsn(opcode, owner, name, desc,itfc);
-//				super.visitInsn(DUP);
-//				super.visitVarInsn(ILOAD, tmpInt);
-//				lvs.freeTmpLV(tmpInt);
-//
-//			}
-//			//stack is now <Integer Integer taint TOP>
-//			super.visitFieldInsn(PUTFIELD, Type.getInternalName(TaintedInt.class), "taint", "I");
-//		} 
-			else
-			super.visitMethodInsn(opcode, owner, name, desc,itfc);
+		} else
+			super.visitMethodInsn(opcode, owner, name, desc, itfc);
 
-		//O T
-		//		if((owner.equals(Type.getInternalName(Integer.class))
-		////				|| owner.equals(Type.getInternalName(String.class))
-		//				|| owner.equals(Type.getInternalName(Byte.class))
-		//				|| owner.equals(Type.getInternalName(Character.class))
-		//				|| owner.equals(Type.getInternalName(Short.class))
-		////				|| owner.equals(Type.getInternalName(Long.class))
-		//				|| owner.equals(Type.getInternalName(Float.class))
-		////				|| owner.equals(Type.getInternalName(Double.class))
-		//				)//TODO support strings and long/double
-		//				&& name.equals("valueOf$$PHOSPHORTAGGED") && nArgs == 1 && !argIsStr)		{
-		//			super.visitInsn(Opcodes.DUP_X1); //O T O
-		//			super.visitInsn(Opcodes.SWAP);//T O O
-		////			if(owner.equals("java/lang/String"))
-		////			{
-		//				//Initialize the taint array so each element is the 
-		//				//TODO i think we were supposed to do something here and i forgot.
-		////				super.visitFieldInsn(Opcodes.PUTFIELD, owner, "value"+TaintUtils.TAINT_FIELD, "[I");
-		////			}
-		////			else
-		//			super.visitFieldInsn(Opcodes.PUTFIELD, owner, TaintUtils.TAINT_FIELD, "I");
-		//		}
 		//TODO boxing the other way maybe too?
 		//TODO handle situations with radix param
 		if(followedByFrame)
 			followedByFrame = false;
 	}
-
 }
--- Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/StringTaintVerifyingMV.java
+++ Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/StringTaintVerifyingMV.java
@@ -15,113 +15,131 @@ import org.objectweb.asm.commons.InstructionAdapter;
 import org.objectweb.asm.tree.FrameNode;
 
 public class StringTaintVerifyingMV extends MethodVisitor implements Opcodes{
-boolean implementsSerializable;
-NeverNullArgAnalyzerAdapter analyzer;
-	public StringTaintVerifyingMV(MethodVisitor mv,boolean implementsSerializable, NeverNullArgAnalyzerAdapter analyzer) {
+	boolean implementsSerializable;
+	NeverNullArgAnalyzerAdapter analyzer;
+	HashSet<String> checkedThisFrame = new HashSet<String>();
+	boolean ignoretaints = false;
+
+	public StringTaintVerifyingMV(MethodVisitor mv, 
+			boolean implementsSerializable, NeverNullArgAnalyzerAdapter analyzer) {
 		super(Opcodes.ASM5,mv);
 		this.analyzer = analyzer;
 		this.implementsSerializable=implementsSerializable;
 	}
-	HashSet<String> checkedThisFrame = new HashSet<String>();
+
 	@Override
-	public void visitFrame(int type, int nLocal, Object[] local, int nStack, Object[] stack) {
+	public void visitFrame(int type, int nLocal,
+			Object[] local, int nStack, Object[] stack) {
 		checkedThisFrame = new HashSet<String>();
 		super.visitFrame(type, nLocal, local, nStack, stack);
 	}
-	
-	boolean ignoretaints = false;
+
 	@Override
 	public void visitInsn(int opcode) {
 		if(opcode == TaintUtils.DONT_LOAD_TAINT)
 			ignoretaints = !ignoretaints;
 		super.visitInsn(opcode);
 	}
+
 	@Override
 	public void visitFieldInsn(int opcode, String owner, String name, String desc) {
-		if(ignoretaints)
-		{
+		if (ignoretaints) {
 			super.visitFieldInsn(opcode, owner, name, desc);
 			return;
 		}
+
 		Type t = Type.getType(desc);
-		if(opcode == Opcodes.GETFIELD && !Instrumenter.isIgnoredClass(owner) && t.getSort() == Type.ARRAY && !name.endsWith(TaintUtils.TAINT_FIELD) && !name.equals("taint") && 
-				t.getElementType().getSort() != Type.OBJECT && t.getDimensions() == 1 && !checkedThisFrame.contains(owner+"."+name)
-				&& (owner.equals("java/lang/String") || implementsSerializable)
-				)
-		{
-//			checkedThisFrame.add(owner+"."+name);
-			
+		if(opcode == Opcodes.GETFIELD
+				&& !Instrumenter.isIgnoredClass(owner)
+				&& t.getSort() == Type.ARRAY
+				&& !name.endsWith(TaintUtils.TAINT_FIELD)
+				&& !name.equals("taint")
+				&& t.getElementType().getSort() != Type.OBJECT
+				&& t.getDimensions() == 1
+				&& !checkedThisFrame.contains(owner+"."+name)
+				&& (owner.equals("java/lang/String")
+					|| implementsSerializable)) {
+
 			super.visitInsn(SWAP);
 			super.visitInsn(POP);
-			
-			//Make sure that it's been initialized
+
+			// Make sure that it's been initialized
 			Label isOK = new Label();
 			Label doInit = new Label();
-			
+
 			FrameNode fn1 = TaintAdapter.getCurrentFrameNode(analyzer);
 			super.visitInsn(DUP);
 			super.visitFieldInsn(opcode, owner, name,desc);
 			super.visitJumpInsn(IFNULL, isOK); //if value is null, do nothing
-			
+
 			super.visitInsn(DUP);
-			super.visitFieldInsn(opcode, owner, name+TaintUtils.TAINT_FIELD, Configuration.TAINT_TAG_ARRAYDESC);
+			super.visitFieldInsn(opcode, owner,
+					name+TaintUtils.TAINT_FIELD, Configuration.TAINT_TAG_ARRAYDESC);
 			super.visitJumpInsn(IFNULL, doInit); //if taint is null, def init
-			//if taint is not null, check the length
+			// if taint is not null, check the length
 			super.visitInsn(DUP); // O O
 			super.visitInsn(DUP); // O O O
 			super.visitFieldInsn(opcode, owner, name,desc);
 			super.visitInsn(ARRAYLENGTH);
 			super.visitInsn(SWAP);
-			super.visitFieldInsn(opcode, owner, name+TaintUtils.TAINT_FIELD, Configuration.TAINT_TAG_ARRAYDESC);
+			super.visitFieldInsn(opcode, owner,
+					name+TaintUtils.TAINT_FIELD, Configuration.TAINT_TAG_ARRAYDESC);
 			super.visitInsn(ARRAYLENGTH);
-			super.visitJumpInsn(IF_ICMPLE, isOK); //if taint is shorter than value, reinit it
+			super.visitJumpInsn(IF_ICMPLE, isOK); // if taint is shorter than value, reinit it
 			super.visitLabel(doInit);
 			TaintAdapter.acceptFn(fn1, this);
 			super.visitInsn(DUP); // O O
 			super.visitInsn(DUP); // O O O
 			super.visitFieldInsn(opcode, owner, name, desc); //O O A
 			super.visitInsn(ARRAYLENGTH);//O O L
-			if (!Configuration.MULTI_TAINTING)
-				super.visitIntInsn(NEWARRAY, T_INT);//O O A
-			else
+
+			if (Configuration.MULTI_TAINTING)
 				super.visitTypeInsn(ANEWARRAY, Configuration.TAINT_TAG_INTERNAL_NAME);
-			super.visitFieldInsn(PUTFIELD, owner, name+TaintUtils.TAINT_FIELD, Configuration.TAINT_TAG_ARRAYDESC); // O
+			else
+				super.visitIntInsn(NEWARRAY, T_INT);//O O A
+
+			super.visitFieldInsn(PUTFIELD, owner,
+					name+TaintUtils.TAINT_FIELD, Configuration.TAINT_TAG_ARRAYDESC); // O
 			super.visitLabel(isOK);
 			TaintAdapter.acceptFn(fn1, this);
 			//O
 			super.visitInsn(DUP);
-			super.visitFieldInsn(opcode, owner, name+TaintUtils.TAINT_FIELD, Configuration.TAINT_TAG_ARRAYDESC);
+			super.visitFieldInsn(opcode, owner,
+					name+TaintUtils.TAINT_FIELD, Configuration.TAINT_TAG_ARRAYDESC);
 			super.visitInsn(SWAP);
 			super.visitFieldInsn(opcode, owner, name, desc);
-
 			return;
-		}
-		else if(opcode == Opcodes.GETFIELD && !Instrumenter.isIgnoredClass(owner) && t.getSort() == Type.ARRAY && !name.endsWith(TaintUtils.TAINT_FIELD) && !name.equals("taint") && 
-				t.getElementType().getSort() != Type.OBJECT && t.getDimensions() == 2 && !checkedThisFrame.contains(owner+"."+name))
-		{
+		} else if (opcode == Opcodes.GETFIELD
+					&& !Instrumenter.isIgnoredClass(owner)
+					&& t.getSort() == Type.ARRAY
+					&& !name.endsWith(TaintUtils.TAINT_FIELD)
+					&& !name.equals("taint")
+					&& t.getElementType().getSort() != Type.OBJECT
+					&& t.getDimensions() == 2
+					&& !checkedThisFrame.contains(owner+"."+name)) {
+
 			super.visitInsn(SWAP);
 			super.visitInsn(POP);
-			
-			//Make sure that it's been initialized
+
+			// Make sure that it's been initialized
 			Label isOK = new Label();
 			Label doInit = new Label();
 
 			FrameNode fn1 = TaintAdapter.getCurrentFrameNode(analyzer);
-
 			super.visitInsn(DUP);
 			super.visitFieldInsn(opcode, owner, name,desc);
 			super.visitJumpInsn(IFNULL, isOK); //if value is null, do nothing
-			
+
 			super.visitInsn(DUP);
-			super.visitFieldInsn(opcode, owner, name+TaintUtils.TAINT_FIELD, "[[I");
+			super.visitFieldInsn(opcode, owner, name + TaintUtils.TAINT_FIELD, "[[I");
 			super.visitJumpInsn(IFNULL, doInit); //if taint is null, def init
-			//if taint is not null, check the length
+			// if taint is not null, check the length
 			super.visitInsn(DUP); // O O
 			super.visitInsn(DUP); // O O O
 			super.visitFieldInsn(opcode, owner, name,desc);
 			super.visitInsn(ARRAYLENGTH);
 			super.visitInsn(SWAP);
-			super.visitFieldInsn(opcode, owner, name+TaintUtils.TAINT_FIELD, "[[I");
+			super.visitFieldInsn(opcode, owner, name + TaintUtils.TAINT_FIELD, "[[I");
 			super.visitInsn(ARRAYLENGTH);
 			super.visitJumpInsn(IF_ICMPLE, isOK); //if taint is shorter than value, reinit it
 			super.visitLabel(doInit);
@@ -132,34 +150,42 @@ NeverNullArgAnalyzerAdapter analyzer;
 			super.visitInsn(DUP);
 			super.visitInsn(ARRAYLENGTH);//O O A L
 			super.visitMultiANewArrayInsn("[[I", 1); //O O A TA
-			super.visitMethodInsn(INVOKESTATIC, Type.getInternalName(TaintUtils.class), "create2DTaintArray", "(Ljava/lang/Object;[[I)[[I",false);
-			super.visitFieldInsn(PUTFIELD, owner, name+TaintUtils.TAINT_FIELD, "[[I"); // O
+			super.visitMethodInsn(INVOKESTATIC,
+					Type.getInternalName(TaintUtils.class),
+					"create2DTaintArray",
+					"(Ljava/lang/Object;[[I)[[I",
+					false);
+			super.visitFieldInsn(PUTFIELD, owner, name + TaintUtils.TAINT_FIELD, "[[I"); // O
 			super.visitLabel(isOK);
 			TaintAdapter.acceptFn(fn1, this);
-			//O
+			// O
 			super.visitInsn(DUP);
-			super.visitFieldInsn(opcode, owner, name+TaintUtils.TAINT_FIELD, "[[I");
+			super.visitFieldInsn(opcode, owner, name + TaintUtils.TAINT_FIELD, "[[I");
 			super.visitInsn(SWAP);
 			super.visitFieldInsn(opcode, owner, name, desc);
 			return;
-		}
-		else if(opcode == Opcodes.GETFIELD && !Instrumenter.isIgnoredClass(owner) && t.getSort() == Type.ARRAY && !name.endsWith(TaintUtils.TAINT_FIELD) && !name.equals("taint") && 
-				t.getElementType().getSort() != Type.OBJECT && t.getDimensions() == 3 && !checkedThisFrame.contains(owner+"."+name))
-		{
+		} else if (opcode == Opcodes.GETFIELD
+					&& !Instrumenter.isIgnoredClass(owner)
+					&& t.getSort() == Type.ARRAY
+					&& !name.endsWith(TaintUtils.TAINT_FIELD)
+					&& !name.equals("taint")
+					&& t.getElementType().getSort() != Type.OBJECT
+					&& t.getDimensions() == 3
+					&& !checkedThisFrame.contains(owner + "." + name)) {
+
 			super.visitInsn(SWAP);
 			super.visitInsn(POP);
-			
+
 			//Make sure that it's been initialized
 			Label isOK = new Label();
 			Label doInit = new Label();
-			
+
 			FrameNode fn1 = TaintAdapter.getCurrentFrameNode(analyzer);
 			super.visitInsn(DUP);
 			super.visitFieldInsn(opcode, owner, name,desc);
 			super.visitJumpInsn(IFNULL, isOK); //if value is null, do nothing
-			
 			super.visitInsn(DUP);
-			super.visitFieldInsn(opcode, owner, name+TaintUtils.TAINT_FIELD, "[[[I");
+			super.visitFieldInsn(opcode, owner, name + TaintUtils.TAINT_FIELD, "[[[I");
 			super.visitJumpInsn(IFNULL, doInit); //if taint is null, def init
 			//if taint is not null, check the length
 			super.visitInsn(DUP); // O O
@@ -167,7 +193,7 @@ NeverNullArgAnalyzerAdapter analyzer;
 			super.visitFieldInsn(opcode, owner, name,desc);
 			super.visitInsn(ARRAYLENGTH);
 			super.visitInsn(SWAP);
-			super.visitFieldInsn(opcode, owner, name+TaintUtils.TAINT_FIELD, "[[[I");
+			super.visitFieldInsn(opcode, owner, name + TaintUtils.TAINT_FIELD, "[[[I");
 			super.visitInsn(ARRAYLENGTH);
 			super.visitJumpInsn(IF_ICMPLE, isOK); //if taint is shorter than value, reinit it
 			super.visitLabel(doInit);
@@ -178,18 +204,21 @@ NeverNullArgAnalyzerAdapter analyzer;
 			super.visitInsn(DUP);
 			super.visitInsn(ARRAYLENGTH);//O O A L
 			super.visitMultiANewArrayInsn("[[[I", 1); //O O A TA
-			super.visitMethodInsn(INVOKESTATIC, Type.getInternalName(TaintUtils.class), "create3DTaintArray", "(Ljava/lang/Object;[[[I)[[[I",false);
-			super.visitFieldInsn(PUTFIELD, owner, name+TaintUtils.TAINT_FIELD, "[[[I"); // O
+			super.visitMethodInsn(INVOKESTATIC,
+					Type.getInternalName(TaintUtils.class),
+					"create3DTaintArray",
+					"(Ljava/lang/Object;[[[I)[[[I",
+					false);
+			super.visitFieldInsn(PUTFIELD, owner, name + TaintUtils.TAINT_FIELD, "[[[I"); // O
 			super.visitLabel(isOK);
 			TaintAdapter.acceptFn(fn1, this);
 			//O
 			super.visitInsn(DUP);
-			super.visitFieldInsn(opcode, owner, name+TaintUtils.TAINT_FIELD, "[[[I");
+			super.visitFieldInsn(opcode, owner, name + TaintUtils.TAINT_FIELD, "[[[I");
 			super.visitInsn(SWAP);
 			super.visitFieldInsn(opcode, owner, name, desc);
 			return;
-		}
-		else
+		} else
 			super.visitFieldInsn(opcode, owner, name, desc);
 	}
 }
