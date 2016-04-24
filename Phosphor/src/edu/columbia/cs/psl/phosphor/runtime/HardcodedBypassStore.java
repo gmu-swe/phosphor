0b5a6d98847e8bb99f4c75df51aa6acfffde697b
dongmu <wangzilinux@gmail.com>
2016-04-24 16:59:26 -0400

    format ArrayHelper.java, ArrayReflectionMasker.java, HardcodedBypassStore.java, MultiTainter.java, RuntimeReflectionPropogator.java and TaintChecker.java

--- Phosphor/src/edu/columbia/cs/psl/phosphor/runtime/HardcodedBypassStore.java
+++ Phosphor/src/edu/columbia/cs/psl/phosphor/runtime/HardcodedBypassStore.java
@@ -1,32 +1,29 @@
 package edu.columbia.cs.psl.phosphor.runtime;
 
-
 import java.lang.reflect.Array;
 
 import edu.columbia.cs.psl.phosphor.Configuration;
 import edu.columbia.cs.psl.phosphor.struct.ArrayList;
 
-
 public class HardcodedBypassStore {
 	static ArrayList<Object> vals = new ArrayList<Object>();
 	
-	public static final Object get(int i)
-	{
+	public static final Object get(int i) {
 		if(i == -1)
 			return null;
 		synchronized (vals) {
 			return vals.get(i);
 		}
 	}
-	public static final Object[] get(int[] i)
-	{
-		if(i == null)
+
+	public static final Object[] get(int[] i) {
+		if (i == null)
 			return null;
 		Object[] ret = (Object[]) Array.newInstance(Configuration.TAINT_TAG_OBJ_CLASS, i.length);
+
 		synchronized (vals) {
-			for(int j = 0; j < i.length; j++)
-			{
-				if(i[j] > 0)
+			for (int j = 0; j < i.length; j++) {
+				if (i[j] > 0)
 					ret[j] = vals.get(j);
 				else
 					ret[j] = null;
@@ -34,29 +31,26 @@ public class HardcodedBypassStore {
 		}
 		return ret;
 	}
-	public static final int add(Object a)
-	{
-		if(a == null)
+
+	public static final int add(Object a) {
+		if (a == null)
 			return -1;
 		synchronized (vals) {
 			vals.add(a);
 			return vals.size() - 1;
 		}
 	}
-	public static final int[] add(Object a[])
-	{
-		if(a == null)
+
+	public static final int[] add(Object a[]) {
+		if (a == null)
 			return null;
 		int[] ret = new int[a.length];
 		synchronized (vals) {
-			for(int j = 0; j < a.length; j++)
-			{
-				if(a[j] != null)
-				{
+			for (int j = 0; j < a.length; j++) {
+				if (a[j] != null) {
 					vals.add(a[j]);
 					ret[j] = vals.size()-1;
-				}
-				else
+				} else
 					ret[j] = 0;
 			}
 		}
