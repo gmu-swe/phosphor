8b2cdf532074c3f885877eea2b9c955a97238225
Jonathan Bell <jbell@cs.columbia.edu>
2016-09-15 14:01:40 -0400

    Format test case to actually be a failsafe test

--- Phosphor/test/edu/columbia/cs/psl/test/phosphor/PrimitiveTaintObjTagITCase.java
+++ Phosphor/test/edu/columbia/cs/psl/test/phosphor/PrimitiveTaintObjTagITCase.java
@@ -1,18 +1,20 @@
 package edu.columbia.cs.psl.test.phosphor;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
+
 
 import edu.columbia.cs.psl.phosphor.runtime.MultiTainter;
 import edu.columbia.cs.psl.phosphor.runtime.Taint;
-import junit.framework.TestCase;
 
-public class PrimitiveTaintIT extends TestCase {
+public class PrimitiveTaintObjTagITCase {
 	
 	@Test
 	public void testPrimitiveTaint() {
 		int i = MultiTainter.taintedInt(1, "test1");
 		Taint t = MultiTainter.getTaint(i);
-		assertEquals(t.getLabel(), "test1");
+		System.out.println(t);
+		assertEquals("test1", t.getLabel());
 	}
 	
 	@Test
@@ -20,7 +22,14 @@ public class PrimitiveTaintIT extends TestCase {
 		int[] arr = {1, 2, 3};
 		arr = MultiTainter.taintedIntArray(arr, "test2");
 		Taint t = MultiTainter.getTaint(arr[0]);
-		assertEquals(t.getLabel(), "test2");
+		System.out.println(t);
+		assertEquals("test2", t.getLabel());
 	}
-
+	
+	public static void main(String[] args) {
+		int i = MultiTainter.taintedInt(1, "test1");
+		Taint t = MultiTainter.getTaint(i);
+		System.out.println(t);
+	}
+	
 }
