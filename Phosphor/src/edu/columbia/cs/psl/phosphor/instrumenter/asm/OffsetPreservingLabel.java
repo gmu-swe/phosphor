0ec6105c74a415d63e01a7420d76f39d657c0616
Jonathan Bell <jbell@cs.columbia.edu>
2017-01-19 11:34:53 -0500

    Add missing files

---
+++ Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/asm/OffsetPreservingLabel.java
@@ -0,0 +1,14 @@
+package edu.columbia.cs.psl.phosphor.instrumenter.asm;
+
+import org.objectweb.asm.Label;
+
+public class OffsetPreservingLabel extends Label {
+	private int originalPosition;
+	public OffsetPreservingLabel(int originalPosition)
+	{
+		this.originalPosition = originalPosition;
+	}
+	public int getOriginalPosition() {
+		return originalPosition;
+	}
+}
