70c713014e5fabcbdb23ee5a67a801cac810ad27
Jonathan Bell <jon@jonbell.net>
2018-01-04 10:09:39 -0500

    some lambda reflection fixers

--- Phosphor/test/edu/columbia/cs/psl/test/phosphor/LambdaObjTagITCase.java
+++ Phosphor/test/edu/columbia/cs/psl/test/phosphor/LambdaObjTagITCase.java
@@ -1,5 +1,12 @@
 package edu.columbia.cs.psl.test.phosphor;
 
+import static java.security.AccessController.doPrivileged;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
+
 import org.junit.Test;
 
 public class LambdaObjTagITCase {
@@ -7,5 +14,24 @@ public class LambdaObjTagITCase {
 	public void testEmptyLambda() throws Exception {
 		Runnable r = () -> {
 		};
+	}
+	
+	@Test
+	public void testLambdaIntArg() throws Exception {
+		intArg(new int[10]);
+	}
+	
+	void initStreams(int[] arg)
+	{
+	}
+	void intArg(int[] arg)
+	{
+		 try {
+	            doPrivileged((PrivilegedExceptionAction<Void>) () -> {
+	                initStreams(arg);
+	                return null;
+	            });
+	        } catch (PrivilegedActionException ex) {
+	        }
 	}
 }
