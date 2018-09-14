b54b8d2a1260d3b4bf12a42305a178a96f6c2952
Miguel Velez <miguelvelez@mijecu25.com>
2018-09-14 08:42:04 -0400

    Implemented disabling the branch not taken in control tracking (#65)
    * added without branch not taken option
    
    * implemented disabling branch not taken analysis
    
    * added without branch not taken option in premain
    
    * added early return if without branch not taken is selected

--- Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/TaintPassingMV.java
+++ Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/TaintPassingMV.java
@@ -828,6 +828,12 @@ public class TaintPassingMV extends TaintAdapter implements Opcodes {
 
 
 	public void doForceCtrlStores(){
+		if(Configuration.WITHOUT_BRANCH_NOT_TAKEN) {
+			forceCtrlStoreFields.clear();
+			forceCtrlAdd.clear();
+			return;
+		}
+
 		MethodVisitor ta = mv;
 
 		for(String t : exceptionsToMaybeThrow){
@@ -2440,6 +2446,10 @@ public class TaintPassingMV extends TaintAdapter implements Opcodes {
 			return;
 		}
 		if (opcode == TaintUtils.FORCE_CTRL_STORE) {
+			if(Configuration.WITHOUT_BRANCH_NOT_TAKEN) {
+				return;
+			}
+
 			//If there is anything on the stack right now, apply the current marker to it
 			if (analyzer.stack.isEmpty() || topOfStackIsNull())
 				return;
