5a174246b0562b213afb720f877f5ca5898b445c
katherine-hough <khough2@gmu.edu>
2019-03-17 20:24:07 -0400

    * Fixed how sources taint array arguments (specifically object arrays and 2+ dimensional primitive arrays) so that autoTaint is used instead of combineTaints on array. Added code to help TaintSourceWrapper.autoTaint to deal with arrays in a reasonable fashion. * Sources now taint arguments before returning in addition to at the beginning of the method in a similar fashion to taintThrough methods. * Added test cases that were previously failing.

--- Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/SourceTaintingMV.java
+++ Phosphor/src/edu/columbia/cs/psl/phosphor/instrumenter/SourceTaintingMV.java
@@ -39,49 +39,60 @@ public class SourceTaintingMV extends MethodVisitor implements Opcodes {
 		}
 	}
 
-	@Override
-	public void visitCode() {
-		super.visitCode();
+	/* Adds code to make a call to autoTaint. Supplies the specified int as the argument index. Check that the value returned
+	* by the call can be cast to the class with the specified internal name. */
+	private void callAutoTaint(int argIndex, String internalName) {
+		super.visitFieldInsn(GETSTATIC, Type.getInternalName(Configuration.class), "autoTainter", Type.getDescriptor(TaintSourceWrapper.class));
+		super.visitInsn(SWAP);
+		super.visitLdcInsn(lbl);
+		super.visitIntInsn(BIPUSH, argIndex);
+		super.visitMethodInsn(INVOKEVIRTUAL, Type.getInternalName(TaintSourceWrapper.class), "autoTaint", "(Ljava/lang/Object;Ljava/lang/String;I)Ljava/lang/Object;", false);
+		super.visitTypeInsn(CHECKCAST, internalName);
+	}
+
+	/* Adds code to taint the arguments passed to this method. */
+	private void autoTaintArguments() {
 		Type[] args = Type.getArgumentTypes(desc);
 		int idx = isStatic ? 0 : 1; // skip over the "this" argument for non-static methods
 		for (int i = 0; i < args.length; i++) {
-			if (args[i].getSort() == Type.OBJECT) {
-				super.visitVarInsn(ALOAD, idx);
-				if(Configuration.MULTI_TAINTING) {
-					super.visitFieldInsn(GETSTATIC, Type.getInternalName(Configuration.class), "autoTainter", Type.getDescriptor(TaintSourceWrapper.class));
-					super.visitInsn(SWAP);
-					super.visitLdcInsn(lbl);
-					super.visitIntInsn(BIPUSH, i);
-					super.visitMethodInsn(INVOKEVIRTUAL, Type.getInternalName(TaintSourceWrapper.class), "autoTaint", "(Ljava/lang/Object;Ljava/lang/String;I)Ljava/lang/Object;", false);
-					super.visitTypeInsn(CHECKCAST, args[i].getInternalName());
-					super.visitVarInsn(ASTORE, idx);
-				} else {
+			if(Configuration.MULTI_TAINTING) {
+				if(args[i].getSort() == Type.OBJECT || args[i].getSort() == Type.ARRAY && args[i].getElementType().getSort() == Type.OBJECT) {
+					super.visitVarInsn(ALOAD, idx); // load the argument onto the stack
+					callAutoTaint(i, args[i].getInternalName());
+					super.visitVarInsn(ASTORE, idx); // replace the argument with the return of autoTaint
+				}
+			} else {
+				if (args[i].getSort() == Type.OBJECT) {
 					// Int-tags
+                    super.visitVarInsn(ALOAD, idx);
 					loadSourceLblAndMakeTaint();
 					super.visitMethodInsn(INVOKESTATIC, Type.getInternalName(TaintChecker.class), "setTaints", "(Ljava/lang/Object;" + Configuration.TAINT_TAG_DESC + ")V", false);
+				} else if (args[i].getSort() == Type.ARRAY && args[i].getElementType().getSort() == Type.OBJECT) {
+					super.visitVarInsn(ALOAD, idx);
+					loadSourceLblAndMakeTaint();
+					super.visitMethodInsn(INVOKEVIRTUAL, Type.getInternalName(TaintSourceWrapper.class), "combineTaintsOnArray", "(Ljava/lang/Object;" + Configuration.TAINT_TAG_DESC + ")V", false);
 				}
-			} else if (args[i].getSort() == Type.ARRAY
-					&& args[i].getElementType().getSort() == Type.OBJECT) {
-				super.visitVarInsn(ALOAD, idx);
-				loadSourceLblAndMakeTaint();
-				super.visitMethodInsn(INVOKEVIRTUAL, Type.getInternalName(TaintSourceWrapper.class), "combineTaintsOnArray", "(Ljava/lang/Object;" + Configuration.TAINT_TAG_DESC + ")V", false);
 			}
 			idx += args[i].getSize();
 		}
 	}
 
 	@Override
+	public void visitCode() {
+		super.visitCode();
+		autoTaintArguments();
+	}
+
+	@Override
 	public void visitInsn(int opcode) {
+		if (TaintUtils.isReturnOpcode(opcode)) {
+			autoTaintArguments();
+		}
 		if (opcode == ARETURN) {
 			Type boxedReturnType = Type.getReturnType(this.desc);
 			if(Configuration.MULTI_TAINTING && origReturnType.getSort() != Type.VOID) {
-				super.visitFieldInsn(GETSTATIC, Type.getInternalName(Configuration.class), "autoTainter", Type.getDescriptor(TaintSourceWrapper.class));
-				super.visitInsn(SWAP);
-				super.visitLdcInsn(lbl);
-				super.visitInsn(ICONST_M1);
-				super.visitMethodInsn(INVOKEVIRTUAL, Type.getInternalName(TaintSourceWrapper.class), "autoTaint", "(Ljava/lang/Object;Ljava/lang/String;I)Ljava/lang/Object;", false);
-				super.visitTypeInsn(CHECKCAST, boxedReturnType.getInternalName());
-			} else if (origReturnType.getSort() != Type.VOID){
+				callAutoTaint(-1, boxedReturnType.getInternalName());
+			} else if (origReturnType.getSort() != Type.VOID) {
 				// Int tags
 				super.visitInsn(DUP);
 				loadSourceLblAndMakeTaint();
