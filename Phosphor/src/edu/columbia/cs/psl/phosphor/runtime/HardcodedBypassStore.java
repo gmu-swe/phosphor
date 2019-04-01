f013e6b2a0961323865cb146f413b92b824316b0
Jonathan Bell <jon@jonbell.net>
2019-04-01 17:14:19 -0400

    Garbage collect Taint objects that are retained for the primitive types that we have to use a side-list to store the Taint objects. Significantly improves performance for multi-taint with lots of tainted Long and Integer objects.

--- Phosphor/src/edu/columbia/cs/psl/phosphor/runtime/HardcodedBypassStore.java
+++ Phosphor/src/edu/columbia/cs/psl/phosphor/runtime/HardcodedBypassStore.java
@@ -1,31 +1,35 @@
 package edu.columbia.cs.psl.phosphor.runtime;
 
-
-import edu.columbia.cs.psl.phosphor.struct.ArrayList;
-
+import edu.columbia.cs.psl.phosphor.struct.GarbageCollectedArrayList;
 
 public class HardcodedBypassStore {
-	static ArrayList<Object> vals = new ArrayList<Object>();
-	
-	static{
+	static GarbageCollectedArrayList<Object> vals = new GarbageCollectedArrayList<>();
+
+	static {
 		vals.add(null);
 	}
-	public static final Object get(int i)
-	{
-		if(i == -1 || i == 0)
+
+	public static final Object get(int i) {
+		if (i == -1 || i == 0)
 			return null;
 		synchronized (vals) {
 			return vals.get(i);
 		}
 	}
-	
-	public static final int add(Object a)
-	{
-		if(a == null)
+
+	public static final int add(Object a) {
+		if (a == null)
 			return -1;
 		synchronized (vals) {
-			vals.add(a);
-			return vals.size() - 1;
+			return vals.add(a);
+		}
+	}
+
+	public static final void free(int i) {
+		if (i >= 0) {
+			synchronized (vals) {
+				vals.free(i);
+			}
 		}
 	}
 }
