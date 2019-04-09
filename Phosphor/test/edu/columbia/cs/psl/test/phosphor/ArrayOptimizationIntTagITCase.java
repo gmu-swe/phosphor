b3a92f4fad1e84b5212900fb396f7128a244e9d4
Aaron Massey <amassey5@gmu.edu>
2019-04-09 19:05:20 -0400

    Add failing tests

---
+++ Phosphor/test/edu/columbia/cs/psl/test/phosphor/ArrayOptimizationIntTagITCase.java
@@ -0,0 +1,41 @@
+package edu.columbia.cs.psl.test.phosphor;
+
+import edu.columbia.cs.psl.phosphor.runtime.MultiTainter;
+import edu.columbia.cs.psl.phosphor.runtime.Taint;
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ArrayOptimizationIntTagITCase  extends BaseMultiTaintClass {
+
+    @Test
+    public void testNewArrayConstantClearsTaint(){
+
+        byte[] b = new byte[10];
+        b[0] = MultiTainter.taintedByte((byte)1,"Foo");
+        b[0] = 10;
+        assertNullOrEmpty(MultiTainter.getTaint(b[0]));
+    }
+
+    @Ignore
+    @Test
+    public void testHashCodeGetsTaint() {
+
+        byte[] b = new byte[10];
+        MultiTainter.taintedObject(b, new Taint("foo"));
+        int taggedHashCode = b.hashCode();
+
+        assertNonNullTaint(taggedHashCode);
+    }
+
+    @Ignore
+    @Test
+    public void testEqualsResultGetsTag() {
+
+        byte[] b = new byte[10];
+        MultiTainter.taintedObject(b, new Taint("foo"));
+        boolean taggedEquals = b.equals(null);
+
+        assertNonNullTaint(MultiTainter.getTaint(taggedEquals));
+    }
+}
+
--- Phosphor/test/edu/columbia/cs/psl/test/phosphor/ArrayOptimizationObjTagITCase.java
+++ Phosphor/test/edu/columbia/cs/psl/test/phosphor/ArrayOptimizationObjTagITCase.java
@@ -5,24 +5,22 @@ import edu.columbia.cs.psl.phosphor.runtime.Taint;
 import org.junit.Test;
 
 public class ArrayOptimizationObjTagITCase extends BaseMultiTaintClass{
+
 	@Test
 	public void testNewArrayConstantClearsTaint(){
 
 		byte[] b = new byte[10];
 		b[0] = MultiTainter.taintedByte((byte)1,"Foo");
 		b[0] = 10;
+
 		assertNullOrEmpty(MultiTainter.getTaint(b[0]));
-
-
 	}
-
 
 	@Test
 	public void testHashCodeGetsTaint() {
 
 		byte[] b = new byte[10];
 		MultiTainter.taintedObject(b, new Taint("foo"));
-
 		int taggedHashCode = b.hashCode();
 
 		assertNonNullTaint(taggedHashCode);
@@ -33,11 +31,8 @@ public class ArrayOptimizationObjTagITCase extends BaseMultiTaintClass{
 
 		byte[] b = new byte[10];
 		MultiTainter.taintedObject(b, new Taint("foo"));
-
 		boolean taggedEquals = b.equals(null);
 
 		assertNonNullTaint(MultiTainter.getTaint(taggedEquals));
 	}
-
-
 }
