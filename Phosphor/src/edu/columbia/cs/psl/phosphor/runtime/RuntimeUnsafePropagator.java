2d4686702f5ae906cad77e2b70f5910f7855119a
katherine-hough <khough2@gmu.edu>
2019-06-21 10:41:10 -0400

    * Added a history of primitive/primitive array offsets to tag offsets to Class instances

--- Phosphor/src/edu/columbia/cs/psl/phosphor/runtime/RuntimeUnsafePropagator.java
+++ Phosphor/src/edu/columbia/cs/psl/phosphor/runtime/RuntimeUnsafePropagator.java
@@ -2,9 +2,9 @@ package edu.columbia.cs.psl.phosphor.runtime;
 
 import edu.columbia.cs.psl.phosphor.Configuration;
 import edu.columbia.cs.psl.phosphor.TaintUtils;
-import edu.columbia.cs.psl.phosphor.struct.ControlTaintTagStack;
 import edu.columbia.cs.psl.phosphor.struct.LazyArrayIntTags;
 import edu.columbia.cs.psl.phosphor.struct.LazyArrayObjTags;
+import edu.columbia.cs.psl.phosphor.struct.SinglyLinkedList;
 import edu.columbia.cs.psl.phosphor.struct.multid.MultiDTaintedArrayWithObjTag;
 import sun.misc.Unsafe;
 
@@ -20,49 +20,66 @@ public class RuntimeUnsafePropagator {
         }
     }
 
-    private static long getTagFieldOffset(Unsafe unsafe, Object o, long offset) {
-        for(Class<?> clazz = o.getClass(); clazz != null; clazz = clazz.getSuperclass()) {
-            Field[] declaredFields = clazz.getDeclaredFields();
-            for(Field field : declaredFields) {
+    private static SinglyLinkedList<OffsetPair> getOffsetPairs(Unsafe unsafe, Class<?> targetClazz) {
+        SinglyLinkedList<OffsetPair> list = new SinglyLinkedList<>();
+        for(Class<?> clazz = targetClazz; clazz != null && !Object.class.equals(clazz); clazz = clazz.getSuperclass()) {
+            for(Field field : clazz.getDeclaredFields()) {
                 try {
                     Class<?> fieldClazz = field.getType();
                     if(fieldClazz.isPrimitive()) {
                         // The field is a primitive
                         long fieldOffset = unsafe.objectFieldOffset(field);
-                        if(fieldOffset == offset) {
-                            // Get the associated taint field
-                            try {
-                                Field taintField = clazz.getField(field.getName() + TaintUtils.TAINT_FIELD);
-                                if (Configuration.MULTI_TAINTING && taintField.getType().equals(Configuration.TAINT_TAG_OBJ_CLASS)) {
-                                    return unsafe.objectFieldOffset(taintField);
-                                } else if (!Configuration.MULTI_TAINTING && taintField.getType().equals(int.class)) {
-                                    return unsafe.objectFieldOffset(taintField);
-                                }
-                            } catch(Exception e) {
-                                return Unsafe.INVALID_FIELD_OFFSET;
+                        // Find the associated taint field's offset
+                        long tagOffset = Unsafe.INVALID_FIELD_OFFSET;
+                        try {
+                            Field taintField = clazz.getField(field.getName() + TaintUtils.TAINT_FIELD);
+                            if (Configuration.MULTI_TAINTING && taintField.getType().equals(Configuration.TAINT_TAG_OBJ_CLASS)) {
+                                tagOffset = unsafe.objectFieldOffset(taintField);
+                            } else if (!Configuration.MULTI_TAINTING && taintField.getType().equals(int.class)) {
+                                tagOffset = unsafe.objectFieldOffset(taintField);
                             }
+                        } catch(Exception e) {
+                            //
                         }
+                        list.enqueue(new OffsetPair(fieldOffset, tagOffset));
                     } else if(fieldClazz.isArray() && fieldClazz.getComponentType().isPrimitive()) {
                         // The fields is a 1D primitive array
                         long fieldOffset = unsafe.objectFieldOffset(field);
-                        if(fieldOffset == offset) {
-                            // Get the associated taint field
-                            try {
-                                Field taintField = clazz.getField(field.getName() + TaintUtils.TAINT_FIELD);
-                                Class<?> taintClazz = taintField.getType();
-                                if (Configuration.MULTI_TAINTING && taintClazz != null && LazyArrayObjTags.class.isAssignableFrom(taintClazz)) {
-                                    return unsafe.objectFieldOffset(taintField);
-                                } else if (!Configuration.MULTI_TAINTING && taintClazz != null && LazyArrayIntTags.class.isAssignableFrom(taintClazz)) {
-                                    return unsafe.objectFieldOffset(taintField);
-                                }
-                            } catch(Exception e) {
-                                return Unsafe.INVALID_FIELD_OFFSET;
+                        // Find the associated taint field's offset
+                        long tagOffset = Unsafe.INVALID_FIELD_OFFSET;
+                        try {
+                            Field taintField = clazz.getField(field.getName() + TaintUtils.TAINT_FIELD);
+                            Class<?> taintClazz = taintField.getType();
+                            if (Configuration.MULTI_TAINTING && taintClazz != null && LazyArrayObjTags.class.isAssignableFrom(taintClazz)) {
+                                tagOffset = unsafe.objectFieldOffset(taintField);
+                            } else if (!Configuration.MULTI_TAINTING && taintClazz != null && LazyArrayIntTags.class.isAssignableFrom(taintClazz)) {
+                                tagOffset = unsafe.objectFieldOffset(taintField);
                             }
+                        } catch(Exception e) {
+                            //
                         }
+                        list.enqueue(new OffsetPair(fieldOffset, tagOffset));
                     }
                 } catch (Exception e) {
                     //
                 }
+            }
+        }
+        return list;
+    }
+
+    /* Returns the offset of the taint field associated with the field at the specified offset for the specified object or
+     * Unsafe.INVALID_FIELD_OFFSET if an associated taint field could not be found. */
+    private static long getTagFieldOffset(Unsafe unsafe, Object o, long offset) {
+        if(o == null || o.getClass() == null) {
+            return Unsafe.INVALID_FIELD_OFFSET;
+        }
+        if(o.getClass().$$PHOSPHOR_OFFSET_CACHE == null) {
+            o.getClass().$$PHOSPHOR_OFFSET_CACHE = getOffsetPairs(unsafe, o.getClass());
+        }
+        for(OffsetPair pair : o.getClass().$$PHOSPHOR_OFFSET_CACHE) {
+            if(pair.origFieldOffset == offset) {
+                return pair.tagFieldOffset;
             }
         }
         return Unsafe.INVALID_FIELD_OFFSET;
@@ -107,5 +124,21 @@ public class RuntimeUnsafePropagator {
                 //
             }
         }
+    }
+
+    public static class OffsetPair {
+        public final long origFieldOffset;
+        public final long tagFieldOffset;
+
+        public OffsetPair(long origFieldOffset, long tagFieldOffset) {
+            this.origFieldOffset = origFieldOffset;
+            this.tagFieldOffset = tagFieldOffset;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("{field @ %d -> tag @ %d}", origFieldOffset, tagFieldOffset);
+        }
+
     }
 }
