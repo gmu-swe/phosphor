a47b22e568c31af57a16e679511eda938db79a43
katherine-hough <khough2@gmu.edu>
2020-02-19 17:12:25 -0500

    * Changed visibility

--- Phosphor/src/main/java/edu/columbia/cs/psl/phosphor/control/graph/EntryPoint.java
+++ Phosphor/src/main/java/edu/columbia/cs/psl/phosphor/control/graph/EntryPoint.java
@@ -3,7 +3,7 @@ package edu.columbia.cs.psl.phosphor.control.graph;
 /**
  * Represents the single point of entry into a control flow graph.
  */
-class EntryPoint extends DummyBasicBlock {
+public class EntryPoint extends DummyBasicBlock {
 
     @Override
     public String toString() {
--- Phosphor/src/main/java/edu/columbia/cs/psl/phosphor/control/graph/ExitPoint.java
+++ Phosphor/src/main/java/edu/columbia/cs/psl/phosphor/control/graph/ExitPoint.java
@@ -3,7 +3,7 @@ package edu.columbia.cs.psl.phosphor.control.graph;
 /**
  * Represents the single point of exit in a control flow graph.
  */
-class ExitPoint extends DummyBasicBlock {
+public class ExitPoint extends DummyBasicBlock {
 
     @Override
     public String toString() {
