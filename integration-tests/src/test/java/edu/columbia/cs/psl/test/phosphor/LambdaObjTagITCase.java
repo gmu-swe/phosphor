541654ce331c965a9dfdf25586dbf6ae482d8baa
katherine-hough <khough2@gmu.edu>
2020-03-28 15:11:21 -0400

    * Added failing test

--- integration-tests/src/test/java/edu/columbia/cs/psl/test/phosphor/LambdaObjTagITCase.java
+++ integration-tests/src/test/java/edu/columbia/cs/psl/test/phosphor/LambdaObjTagITCase.java
@@ -1,69 +1,66 @@
 package edu.columbia.cs.psl.test.phosphor;
 
-import static java.security.AccessController.doPrivileged;
-import static org.junit.Assert.*;
+import edu.columbia.cs.psl.phosphor.runtime.MultiTainter;
+import org.junit.Test;
 
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.BiConsumer;
-import java.util.function.ObjDoubleConsumer;
-import java.util.function.Supplier;
+import java.util.function.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import org.junit.Test;
+import static java.security.AccessController.doPrivileged;
+import static org.junit.Assert.assertEquals;
 
-public class LambdaObjTagITCase extends BasePhosphorTest{
+public class LambdaObjTagITCase extends BaseMultiTaintClass {
 
 	@Test
-	public void testDoubleConsumer() throws Exception{
+	public void testDoubleConsumer() {
 		ObjDoubleConsumer<double[]> consumer = (ll, l) -> {
 			ll[0] += l;
 		};
 		consumer.accept(new double[1], 1.0);
-
-		BiConsumer<double[], double[]> consumer2 = (ll, rr) -> {
-			ll[0] += rr[0];
-		};
+		BiConsumer<double[], double[]> consumer2 = (ll, rr) -> ll[0] += rr[0];
 		consumer2.accept(new double[1], new double[1]);
 	}
+
 	@Test
-	public void testCollectors() throws Exception{
+	public void testCollectors() {
 		List<String> givenList = Arrays.asList("a", "bb", "ccc", "dd");
 		givenList.stream().collect(Collectors.toList());
-
-	}
-	@Test
-	public void testIntStreamsDontCrash() throws Exception {
-		int sum = IntStream.of(1, 2, 3, 4, 5).sum(); //creates a bunch of lambdas
 	}
 
 	@Test
-	public void testEmptyLambda() throws Exception {
+	public void testIntStreamsDoNotCrash() {
+		int sum = IntStream.of(1, 2, 3, 4, 5).sum(); // creates a bunch of lambdas
+	}
+
+	@Test
+	public void testEmptyLambda() {
 		Runnable r = () -> {
 		};
 	}
 
 	@Test
-	public void testZeroArgVoidWrappedLambda() throws Exception {
+	public void testZeroArgVoidWrappedLambda() {
 		int[] i = new int[1];
 		Runnable r = () -> {
 			i[0] += 10;
 		};
 		r.run();
-		assertTrue("Expected runnable constructed using lambda to run.", i[0] == 10);
+		assertEquals("Expected runnable constructed using lambda to run.", 10, i[0]);
 	}
 
 	@Test
-	public void testLambdaIntArg() throws Exception {
+	public void testLambdaIntArg() {
 		intArg(new int[10]);
 	}
 
 	@Test
-	public void testLocalDateTimeDoesntCrash() throws Exception {
+	public void testLocalDateTimeDoesntCrash() {
 		LocalDateTime.now();
 		//Just testing to make sure no exception
 	}
@@ -77,12 +74,22 @@ public class LambdaObjTagITCase extends BasePhosphorTest{
 				initStreams(arg);
 				return null;
 			});
-		} catch (PrivilegedActionException ex) {
+		} catch(PrivilegedActionException ex) {
+			//
 		}
 	}
+
 	@Test
-	public void testSupplier() throws Exception {
+	public void testSupplier() {
 		Supplier<double[]> supplier = () -> new double[3];
 		double[] d = supplier.get();
+	}
+
+	@Test
+	public void testUnaryOperator() {
+		UnaryOperator<String> f = s -> s;
+		String in = MultiTainter.taintedReference("hello", 0);
+		String out = f.apply(in);
+		assertEquals(in, out);
 	}
 }
