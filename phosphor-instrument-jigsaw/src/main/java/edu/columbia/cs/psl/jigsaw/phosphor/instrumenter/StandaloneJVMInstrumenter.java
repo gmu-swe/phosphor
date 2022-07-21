00f54135a9bafbcf5dc3b5114a7ed0ccd3b09149
Jonathan Bell <jon@jonbell.net>
2022-07-21 08:14:00 -0400

    Refactor java8+ names

---
+++ phosphor-instrument-jigsaw/src/main/java/edu/columbia/cs/psl/jigsaw/phosphor/instrumenter/StandaloneJVMInstrumenter.java
@@ -0,0 +1,6 @@
+package edu.columbia.cs.psl.jigsaw.phosphor.instrumenter;
+
+public class StandaloneJVMInstrumenter {
+    public static void main(String[] args) {
+    }
+}
---
+++ phosphor-instrument-jigsaw/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module edu.columbia.cs.psl.jigsaw.phosphor.instrumenter {
+    exports edu.columbia.cs.psl.jigsaw.phosphor.instrumenter;
+    requires jdk.jlink;
+    requires java.instrument;
+}
